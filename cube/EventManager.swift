e4520aa9655c729426e5f9bb18c61752f2e1a327
Ross Huelin <filmstarr@hotmail.com>
2016-03-03 23:56:35 +0000

    Add gameGrid and simple random death scenario.

---
+++ cube/EventManager.swift
@@ -0,0 +1,9 @@
+//
+//  EventManager.swift
+//  cube
+//
+//  Created by Ross Huelin on 03/03/2016.
+//  Copyright © 2016 filmstarr. All rights reserved.
+//
+
+import Foundation
---
+++ cube/gameGrid.swift
@@ -0,0 +1,9 @@
+//
+//  gameGrid.swift
+//  cube
+//
+//  Created by Ross Huelin on 03/03/2016.
+//  Copyright © 2016 filmstarr. All rights reserved.
+//
+
+import Foundation
